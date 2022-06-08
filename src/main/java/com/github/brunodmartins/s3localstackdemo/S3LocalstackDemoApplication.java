74a94c7ba835ce4d83eb33365a39bea0416b3537
brunodmartins <bdm2943@gmail.com>
2022-06-07 23:07:35 -0300

    Initial commit

---
+++ src/main/java/com/github/brunodmartins/s3localstackdemo/S3LocalstackDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.brunodmartins.s3localstackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3LocalstackDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3LocalstackDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/brunodmartins/s3localstackdemo/S3LocalstackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brunodmartins.s3localstackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3LocalstackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
